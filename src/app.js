f9ca119daddf07c5b82f7cc6562d170768579c34
Nitin Mali <nitinmali22@gmail.com>
2019-03-27 15:30:28 +0530

    base

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
 export class App {
 
-  configureRouter(config, router){
+  //default on load its false
+  configMenuVisible = false;
+
+  configureRouter(config, router) {
     config.title = 'Contacts';
     config.map([
-      { route: ['', 'home'], moduleId: 'views/home',   title: 'Home' },
-      { route: 'line',       moduleId: 'views/line',   name:'line',   nav:true, title:'Line Graph' },
-      { route: 'column',     moduleId: 'views/column', name:'column', nav:true, title:'Column Graph' },
-      { route: 'bar',        moduleId: 'views/bar',    name:'bar',    nav:true, title:'Bar Graph' },
-      { route: 'pie',        moduleId: 'views/pie',    name:'pie',    nav:true, title:'Pie Graph' }
+      { route: ['', 'home'], moduleId: 'views/line', title: 'Home' },
+      { route: 'line', moduleId: 'views/line', name: 'line', nav: true, title: 'Line Graph' },
+      { route: 'column', moduleId: 'views/column', name: 'column', nav: true, title: 'Column Graph' },
+      { route: 'bar', moduleId: 'views/bar', name: 'bar', nav: true, title: 'Bar Graph' },
+      { route: 'pie', moduleId: 'views/pie', name: 'pie', nav: true, title: 'Pie Graph' }
     ]);
 
     this.router = router;
+  }
+
+  toggleNav() {
+    if (this.configMenuVisible) {
+      document.getElementById('mySidenav').style.width = '0';
+      document.getElementById('main').style.marginLeft = '0';
+
+    } else {
+      document.getElementById('mySidenav').style.width = '250px';
+      document.getElementById('main').style.marginLeft = '250px';
+    }
+
+    this.configMenuVisible = !this.configMenuVisible;
   }
 }
